21a0e701692cf75a6499a4b123b64211bb0e148d
alex-aghamyan <alex.aghamyan.work@gmail.com>
2021-10-26 14:37:24 +0400

    project initial setup
    Create project structure, add database as json file.

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+import booksRouter from './routes/books.route';
+
+const app = express();
+const PORT: number = 5000;
+
+app.use(express.json());
+app.use('/api/books', booksRouter);
+
+app.listen(PORT, () => {
+  console.log(`listening at port ${PORT}...`);
+});
